1bbf32f075fad6410ac81fa7811b43c6d2102886
Surya Prakash <suryap@tabtale.com>
2022-06-20 18:27:13 +0530

    Melting mech

--- Assets/DIY_SayCheese/_Scripts/Levels/StepManager/CheeseMeltManager.cs
+++ Assets/DIY_SayCheese/_Scripts/Levels/StepManager/CheeseMeltManager.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Deform;
 
 public class CheeseMeltManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public List<PerlinNoiseDeformer> cheeseMeltDeformers = new List<PerlinNoiseDeformer>();
+    public ParticleSystem cheeseBubbles;
 
-    // Update is called once per frame
+    [Range(0,1f)]
+    public float meltAmount;
+    public float maxAmount = 0.025f;
+    public float maxParticles = 5;
+    bool toChangeValue = true;
+
     void Update()
     {
-        
+        CheeseMeltAmount();
     }
+
+    void CheeseMeltAmount()
+    {
+        if(!toChangeValue) return;
+
+        foreach(PerlinNoiseDeformer deformer in cheeseMeltDeformers)
+        {
+            deformer.MagnitudeScalar = Remap.remap(meltAmount, 0, 1, 0, maxAmount);
+        }
+        ParticleSystem.EmissionModule cheesePfx = cheeseBubbles.emission;
+        cheesePfx.rateOverTime = Remap.remap(meltAmount, 0, 1, 0, maxParticles);
+    }
+
 }
