21f400b720425999df560454f346459fc005f0d6
Surya Prakash <suryap@tabtale.com>
2022-06-20 19:27:48 +0530

    Heat gun mech

--- Assets/DIY_SayCheese/_Scripts/Levels/HeatGun.cs
+++ Assets/DIY_SayCheese/_Scripts/Levels/HeatGun.cs
@@ -4,10 +4,59 @@ using UnityEngine;
 
 public class HeatGun : MonoBehaviour
 {
+
+    public bool isHeating;
+    Collider gunCollider;
+    public Transform rotationLeft;
+    public Transform rotationRight;
+    public ParticleSystem fire;
+    void Start()
+    {
+        gunCollider = GetComponent<Collider>();
+    }
+    void Update()
+    {
+        UpdateRotation();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            gunCollider.enabled = true;
+            if(!fire.isPlaying)
+            {
+                fire.Play();
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            gunCollider.enabled = false;
+            isHeating = false;
+            if (fire.isPlaying)
+            {
+                fire.Stop();
+            }
+        }
+    }
+
+    void UpdateRotation()
+    {
+        transform.parent.transform.rotation = Quaternion.Lerp(rotationLeft.rotation, rotationRight.rotation, Remap.remap(transform.parent.transform.position.x, rotationLeft.position.x, rotationRight.position.x, 0, 1));
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Cheese")
         {
+            isHeating = true;
         }
     }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Cheese")
+        {
+            isHeating = false;
+        }
+    }
+
+
 }
--- Assets/DIY_SayCheese/_Scripts/Levels/StepManager/CheeseMeltManager.cs
+++ Assets/DIY_SayCheese/_Scripts/Levels/StepManager/CheeseMeltManager.cs
@@ -8,27 +8,56 @@ public class CheeseMeltManager : MonoBehaviour
     public List<PerlinNoiseDeformer> cheeseMeltDeformers = new List<PerlinNoiseDeformer>();
     public ParticleSystem cheeseBubbles;
     public HeatGun heatGun;
-    [Range(0,1f)]
+    [Range(0, 1f)]
     public float meltAmount;
+    public float meltSpeed = 1f;
     public float maxAmount = 0.025f;
     public float maxParticles = 5;
-    bool toChangeValue = true;
-
+    bool held = false;
     void Update()
     {
-        CheeseMeltAmount();
+        SetInput();
+        UpdateCheeseAmount();
     }
 
+    void SetInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            held = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            held = false;
+        }
+    }
+    void UpdateCheeseAmount()
+    {
+        if (!held) return;
+
+        if (!heatGun.isHeating) return;
+
+        meltAmount += meltSpeed * Time.deltaTime;
+        meltAmount = Mathf.Clamp(meltAmount, 0, 1);
+        if(meltAmount >= 1)
+        {
+            StepComplete();
+        }
+        CheeseMeltAmount();
+    }
     void CheeseMeltAmount()
     {
-        if(!toChangeValue) return;
-
-        foreach(PerlinNoiseDeformer deformer in cheeseMeltDeformers)
+        foreach (PerlinNoiseDeformer deformer in cheeseMeltDeformers)
         {
             deformer.MagnitudeScalar = Remap.remap(meltAmount, 0, 1, 0, maxAmount);
         }
         ParticleSystem.EmissionModule cheesePfx = cheeseBubbles.emission;
         cheesePfx.rateOverTime = Remap.remap(meltAmount, 0, 1, 0, maxParticles);
+    }
+
+    void StepComplete()
+    {
+        heatGun.transform.parent.gameObject.SetActive(false);
     }
 
 }
